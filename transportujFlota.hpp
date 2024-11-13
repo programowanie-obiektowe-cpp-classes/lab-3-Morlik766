e953dcdb2c39c5e1caa3ab5bf8fbfb0fa698d79d
Mikołaj Orlik <01171673@pw.edu.pl>
2024-11-13 21:02:43 +0100

    zadanko

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,20 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int sumaPrzetransportowanegoTowaru = 0;
+    unsigned int liczbaZaglowcow = 0;
+    if (towar == 0) {
+        return 0;
+    }
+    Stocznia stocznia{};
+    while (sumaPrzetransportowanegoTowaru < towar) {
+        Statek* statek = stocznia();
+        unsigned int przetransportowanyTowar = statek->transportuj();
+        sumaPrzetransportowanegoTowaru += przetransportowanyTowar;
+        if (dynamic_cast< Zaglowiec* >(statek) != nullptr) {
+            ++liczbaZaglowcow; 
+        }
+        delete statek;
+    }
+    return liczbaZaglowcow;
 }
